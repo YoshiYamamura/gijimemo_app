be8b96ba29a379ecc91ac7fcd3f18dc5839e3211
YoshiYamamura <y.yamamura86@gmail.com>
2021-04-01 09:27:46 +0900

    meetings#new, createのコントローラー・ルーティング設定

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -3,4 +3,24 @@ class MeetingsController < ApplicationController
 
   def index
   end
+
+  def new
+    @meeting = Meeting.new
+  end
+
+  def create
+    @meeting = Meeting.new(meeting_params)
+    if @meeting.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:name, :meeting_date, :meeting_time, :place, :attendance, :speech).merge(user_id: current_user.id)
+  end
+
 end
