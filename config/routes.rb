163387eab50d86ffcd0caa93bb89b98f1575381b
YoshiYamamura <y.yamamura86@gmail.com>
2021-04-02 18:59:24 +0900

    議事録削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'meetings#index'
-  resources :meetings, only: [:index, :new, :create, :edit, :update]
+  resources :meetings, only: [:index, :new, :create, :edit, :update, :destroy]
 end
