b4639ffd27efec2057ce4bc17ff9d30710773280
YoshiYamamura <y.yamamura86@gmail.com>
2021-04-04 14:06:38 +0900

    ユーザープロフィールページ作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,30 @@
 class ProfilesController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    if Profile.where(user_id: @user.id).present?
+      @profile = Profile.where(user_id: @user.id)
+    else
+      @profile = Profile.new
+    end
   end
+
   def new
+    @profile = Profile.new
   end
+
   def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to profile_path(@profile), notice: "プロフィールを登録しました。"
+    else
+      render :new
+    end
   end
+
+  private
+
+  def meeting_params
+    params.require(:profile).permit(:family_name, :first_name, :belonging, :self_introduction).merge(user_id: current_user.id)
+  end
+
 end
