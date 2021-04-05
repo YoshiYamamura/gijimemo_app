cdccb93a26ffce03c0ffa9e247fa2889f31a903e
YoshiYamamura <y.yamamura86@gmail.com>
2021-04-05 17:38:21 +0900

    サイドバーのリンク設定

--- app/javascript/meeting_select.js
+++ app/javascript/meeting_select.js
@@ -5,6 +5,8 @@ function meetingSelect() {
   const editBtnLink = document.getElementById('edit_btn_link');
   const deleteBtn = document.getElementById('delete-btn');
   const deleteBtnLink = document.getElementById('delete_btn_link');
+  const accessPremitBtn = document.getElementById('access-permit-btn');
+  const accessPremitBtnLink = document.getElementById('access_permit_btn_link');
   const showBtn = document.getElementById('show-btn');
   const showBtnLink = document.getElementById('show_btn_link');
 
@@ -17,15 +19,18 @@ function meetingSelect() {
         meetingRecordSelected.classList.add('selected');
         //選択（クリック）したmeetingRecordのid取得
         let recordId = meetingRecordSelected.getAttribute('id').replace('meeting-id-','');
-        //編集、削除、閲覧ボタンが活性化
+        //編集、削除、共有設定、閲覧ボタンが活性化
         editBtn.classList.remove('disabled');
         deleteBtn.classList.remove('disabled');
+        accessPremitBtn.classList.remove('disabled');
         showBtn.classList.remove('disabled');
-        //編集、削除、閲覧ボタンのリンク設定
+        //編集、削除、共有設定、閲覧ボタンのリンク設定
         editBtnLink.removeAttribute("href");
         editBtnLink.setAttribute("href", `/meetings/${recordId}/edit`);
         deleteBtnLink.removeAttribute("href");
         deleteBtnLink.setAttribute('href', `/meetings/${recordId}`);
+        accessPremitBtnLink.removeAttribute("href");
+        accessPremitBtnLink.setAttribute('href', `/meetings/${recordId}/access_permits`);
         showBtnLink.removeAttribute("href");
         showBtnLink.setAttribute("href", `/meetings/${recordId}`);
       })
