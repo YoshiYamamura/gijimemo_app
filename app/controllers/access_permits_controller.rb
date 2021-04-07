60952e568beba1bb5a8a780be025ffb2bcd7b8a2
YoshiYamamura <y.yamamura86@gmail.com>
2021-04-07 13:48:05 +0900

    議事録共有設定ページの表示

--- app/controllers/access_permits_controller.rb
+++ app/controllers/access_permits_controller.rb
@@ -1,4 +1,14 @@
 class AccessPermitsController < ApplicationController
   def index
+    @access_permits = AccessPermit.where(meeting: params[:meeting_id])
+    @users = User.where.not(id: current_user.id)
+    @access_permits.each do |access_permit|
+      @users = @users.reject{|user| user.id == access_permit.user_id}
+    end
+    @meeting = Meeting.find(params[:meeting_id])
+  end
+
+  def create
+    binding.pry
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'meetings#index'
   resources :meetings do
-    resources :access_permits, only: :index
+    resources :access_permits, only: [:index, :create]
   end
   resources :profiles, only: [:show, :new, :create, :edit, :update]
 end
