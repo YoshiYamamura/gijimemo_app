b7f2f75cff8303c1c82802d0aff4585ae85ebce4
YoshiYamamura <y.yamamura86@gmail.com>
2021-04-10 18:00:49 +0900

    文字起こし機能　新規作成ページビューファイル・コントローラー

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -1,7 +1,24 @@
 class TranscriptsController < ApplicationController
   def new
+    @transcript = Transcript.new
   end
 
   def create
+    @transcript = Transcript.new(transcript_params)
+    if @transcript.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @transcript = Transcript.find(params[:id])
+  end
+
+  private
+
+  def transcript_params
+    params.require(:transcript).permit(:name, :transcript, :status, :voice_data).merge(user_id: current_user.id)
   end
 end
