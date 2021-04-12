04164f33c200bc181b5d257233adc0934c5f1c14
YoshiYamamura <y.yamamura86@gmail.com>
2021-04-12 20:20:55 +0900

    サイドバーのjavascriptに文字起こしデータ削除を追加、リファクタリング

--- app/javascript/meeting_select.js
+++ app/javascript/meeting_select.js
@@ -9,6 +9,37 @@ function list_select(meetingRecordSelected) {
   return meetingRecordSelected.getAttribute('id').replace('meeting-id-','').replace('transcript-id-',''); //選択（クリック）したRecordのid取得
 }
 
+function button_activate(target, targetlink, recordId, act) {
+
+  const editBtn = document.getElementById('edit-btn');
+  const deleteMeetingBtn = document.getElementById('delete-btn');
+  const accessPermitBtn = document.getElementById('access-permit-btn');
+  const showMeetingBtn = document.getElementById('show-meeting-btn');
+  const showTranscriptBtn = document.getElementById('show-transcript-btn');
+  const deleteTranscriptBtn = document.getElementById('delete-transcript-btn');
+  //act == 1で活性化、0で不活性化
+  if(act == 1){
+    target.classList.remove('disabled');
+    targetlink.removeAttribute("href");
+    if(target == editBtn) {
+      targetlink.setAttribute("href", `/meetings/${recordId}/edit`);
+    }else if(target == deleteMeetingBtn) {
+      targetlink.setAttribute('href', `/meetings/${recordId}`);
+    }else if(target == accessPermitBtn) {
+      targetlink.setAttribute('href', `/meetings/${recordId}/access_permits`);
+    }else if(target == showMeetingBtn) {
+      targetlink.setAttribute("href", `/meetings/${recordId}`);
+    }else if(target == showTranscriptBtn) {
+      targetlink.setAttribute("href", `/transcripts/${recordId}`);
+    }else if(target == deleteTranscriptBtn) {
+      targetlink.setAttribute("href", `/transcripts/${recordId}`);
+    };
+  }else{
+    target.classList.add('disabled');
+    targetlink.removeAttribute("href");
+  };
+}
+
 function meetingSelect() {
 
   const meetingRecordCreate = document.querySelectorAll('.meeting-record-create');
@@ -16,37 +47,29 @@ function meetingSelect() {
   const transcriptRecord = document.querySelectorAll('.transcript-record');
   const editBtn = document.getElementById('edit-btn');
   const editBtnLink = document.getElementById('edit_btn_link');
-  const deleteBtn = document.getElementById('delete-btn');
-  const deleteBtnLink = document.getElementById('delete_btn_link');
-  const accessPremitBtn = document.getElementById('access-permit-btn');
-  const accessPremitBtnLink = document.getElementById('access_permit_btn_link');
+  const deleteMeetingBtn = document.getElementById('delete-btn');
+  const deleteMeetingBtnLink = document.getElementById('delete_btn_link');
+  const accessPermitBtn = document.getElementById('access-permit-btn');
+  const accessPermitBtnLink = document.getElementById('access_permit_btn_link');
   const showMeetingBtn = document.getElementById('show-meeting-btn');
   const showMeetingBtnLink = document.getElementById('show_meeting_btn_link');
   const showTranscriptBtn = document.getElementById('show-transcript-btn');
   const showTranscriptBtnLink = document.getElementById('show_transcript_btn_link');
+  const deleteTranscriptBtn = document.getElementById('delete-transcript-btn');
+  const deleteTranscriptBtnLink = document.getElementById('delete_transcript_btn_link');
 
   //自分が作成した議事録を選択した場合
   meetingRecordCreate.forEach(function(meetingRecordSelected){
     meetingRecordSelected.addEventListener('click', function(){
       let recordId = list_select(meetingRecordSelected);
       //議事録:編集、削除、共有設定、閲覧ボタンが活性化
-      editBtn.classList.remove('disabled');
-      deleteBtn.classList.remove('disabled');
-      accessPremitBtn.classList.remove('disabled');
-      showMeetingBtn.classList.remove('disabled');
-      //文字起こしデータ:閲覧ボタンが不活性化
-      showTranscriptBtn.classList.add('disabled');
-      //議事録:編集、削除、共有設定、閲覧ボタンのリンク設定
-      editBtnLink.removeAttribute("href");
-      editBtnLink.setAttribute("href", `/meetings/${recordId}/edit`);
-      deleteBtnLink.removeAttribute("href");
-      deleteBtnLink.setAttribute('href', `/meetings/${recordId}`);
-      accessPremitBtnLink.removeAttribute("href");
-      accessPremitBtnLink.setAttribute('href', `/meetings/${recordId}/access_permits`);
-      showMeetingBtnLink.removeAttribute("href");
-      showMeetingBtnLink.setAttribute("href", `/meetings/${recordId}`);
-      //文字起こしデータ:閲覧ボタンのリンク削除
-      showTranscriptBtnLink.removeAttribute("href");
+      button_activate(editBtn, editBtnLink, recordId, 1);
+      button_activate(deleteMeetingBtn, deleteMeetingBtnLink, recordId, 1);
+      button_activate(accessPermitBtn, accessPermitBtnLink, recordId, 1);
+      button_activate(showMeetingBtn, showMeetingBtnLink, recordId, 1);
+      //文字起こしデータ:閲覧、削除ボタンが不活性化
+      button_activate(showTranscriptBtn, showTranscriptBtnLink, recordId, 0);
+      button_activate(deleteTranscriptBtn, deleteTranscriptBtnLink, recordId, 0);
     })
   })
 
@@ -54,21 +77,14 @@ function meetingSelect() {
   meetingRecordShow.forEach(function(meetingRecordSelected){
     meetingRecordSelected.addEventListener('click', function(){
       let recordId = list_select(meetingRecordSelected);
-      //議事録:編集、削除、共有設定ボタン,文字起こしデータ:閲覧ボタンが不活性化
-      editBtn.classList.add('disabled');
-      deleteBtn.classList.add('disabled');
-      accessPremitBtn.classList.add('disabled');
-      showTranscriptBtn.classList.add('disabled');
+      //議事録:編集、削除、共有設定ボタン,文字起こしデータ:閲覧、削除ボタンが不活性化
+      button_activate(editBtn, editBtnLink, recordId, 0);
+      button_activate(deleteMeetingBtn, deleteMeetingBtnLink, recordId, 0);
+      button_activate(accessPermitBtn, accessPermitBtnLink, recordId, 0);
+      button_activate(showTranscriptBtn, showTranscriptBtnLink, recordId, 0);
+      button_activate(deleteTranscriptBtn, deleteTranscriptBtnLink, recordId, 0);
       //議事録:閲覧ボタンが活性化
-      showMeetingBtn.classList.remove('disabled');
-      //議事録:編集、削除、共有設定ボタン,文字起こしデータ:閲覧ボタンのリンク削除
-      editBtnLink.removeAttribute("href");
-      deleteBtnLink.removeAttribute("href");
-      accessPremitBtnLink.removeAttribute("href");
-      showTranscriptBtnLink.removeAttribute("href");
-      //議事録:閲覧ボタンのリンク設定
-      showMeetingBtnLink.removeAttribute("href");
-      showMeetingBtnLink.setAttribute("href", `/meetings/${recordId}`);
+      button_activate(showMeetingBtn, showMeetingBtnLink, recordId, 1);
     })
   })
 
@@ -77,20 +93,13 @@ function meetingSelect() {
     meetingRecordSelected.addEventListener('click', function(){
       let recordId = list_select(meetingRecordSelected);
       //議事録:編集、削除、共有設定、閲覧ボタンが不活性化
-      editBtn.classList.add('disabled');
-      deleteBtn.classList.add('disabled');
-      accessPremitBtn.classList.add('disabled');
-      showMeetingBtn.classList.add('disabled');
-      //文字起こしデータ:閲覧ボタンが活性化
-      showTranscriptBtn.classList.remove('disabled');
-      //議事録:編集、削除、共有設定、閲覧ボタンのリンク削除
-      editBtnLink.removeAttribute("href");
-      deleteBtnLink.removeAttribute("href");
-      accessPremitBtnLink.removeAttribute("href");
-      showMeetingBtnLink.removeAttribute("href");
-      //文字起こしデータ:閲覧ボタンのリンク設定
-      showTranscriptBtnLink.removeAttribute("href");
-      showTranscriptBtnLink.setAttribute("href", `/transcripts/${recordId}`);
+      button_activate(editBtn, editBtnLink, recordId, 0);
+      button_activate(deleteMeetingBtn, deleteMeetingBtnLink, recordId, 0);
+      button_activate(accessPermitBtn, accessPermitBtnLink, recordId, 0);
+      button_activate(showMeetingBtn, showMeetingBtnLink, recordId, 0);
+      //文字起こしデータ:閲覧、削除ボタンが活性化
+      button_activate(showTranscriptBtn, showTranscriptBtnLink, recordId, 1);
+      button_activate(deleteTranscriptBtn, deleteTranscriptBtnLink, recordId, 1);
     })
   })
 }
