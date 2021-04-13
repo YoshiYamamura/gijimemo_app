bb9cf25c14075f02f85437b2a3fa3369da23fae2
YoshiYamamura <y.yamamura86@gmail.com>
2021-04-13 20:08:53 +0900

    reactionsコントローラー作成・ルーティング

---
+++ app/controllers/reactions_controller.rb
@@ -0,0 +1,18 @@
+class ReactionsController < ApplicationController
+  def create
+    reaction = Reaction.create(reaction_params)
+    redirect_to meeting_path(reaction.meeting.id)
+  end
+  
+  def destroy
+    reaction = Reaction.find(params[:id])
+    reaction.destroy
+    redirect_to meeting_path(reaction.meeting.id)
+  end
+
+  private
+
+  def reaction_params
+    params.require(:reaction).permit(:comment).merge(user_id: current_user.id, meeting_id: params[:meeting_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'meetings#index'
   resources :meetings do
     resources :access_permits, only: [:index, :create]
+    resources :reactions, only: [:create, :destroy]
   end
   resources :transcripts, only: [:new, :create, :show, :destroy]
   resources :profiles, only: [:show, :new, :create, :edit, :update]
