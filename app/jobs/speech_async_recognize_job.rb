687e4992bf360a43547a1d2b78e5ac50038aa886
YoshiYamamura <y.yamamura86@gmail.com>
2021-04-28 18:29:19 +0900

    Revert "v1p1bata1への切替"
    This reverts commit 3580ffebb7b655439f7b6bf8b78d27b9e5964142.

--- app/jobs/speech_async_recognize_job.rb
+++ app/jobs/speech_async_recognize_job.rb
@@ -8,10 +8,8 @@ class SpeechAsyncRecognizeJob < ApplicationJob
   end
 
   def perform(transcript, language, sub_language, number_of_people, samplerate, channels)
-    #require "google/cloud/speech"
-    #speech = Google::Cloud::Speech.speech
-    require "google/cloud/speech/v1p1bata1"
-    speech = ::Google::Cloud::Speech::V1p1beta1::Speech::Client.new
+    require "google/cloud/speech"
+    speech = Google::Cloud::Speech.speech
 
     storage_path = "gs://gijimemo_bucket/#{transcript.voice_data.key}"
 
